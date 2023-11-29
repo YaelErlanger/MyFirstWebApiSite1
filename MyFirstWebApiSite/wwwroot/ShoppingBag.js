2d644d7aab865923bd4bfc82c39900a37a308358
326659356 <yael05485@gmail.com>
2023-11-29 10:26:57 +0200

    version 29.11.2023

--- MyFirstWebApiSite/wwwroot/ShoppingBag.js
+++ MyFirstWebApiSite/wwwroot/ShoppingBag.js
@@ -34,25 +34,31 @@ const placeOrder = async () => {
     }
     else {
         const order = await createOrder(user);
-        const createdOrder = await postOrder(order);
-        alert("added successfuly!")
+         postOrder(order);
+        
     }
 }
 const postOrder = async (order) => {
-    const res = await fetch("api/Orders", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(order)
-    })
-    if (!res.ok) {
-        alert("failed")
-        return
+    try {
+        const res = await fetch("api/Orders", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(order)
+        })
+        if (!res.ok) {
+            alert("failed")
+            return
+        }
+        const data = await res.json();
+        alert(`your order ${data.orderId} created successfuly`);
+
+        return data;
     }
-    const createdOrder = await res.json();
-    console.log(createdOrder);
-    return createdOrder;
+    catch (e) {
+        console.log(e);
+    }
 }
 
 const createOrder = async (stringUser) => {
@@ -60,81 +66,21 @@ const createOrder = async (stringUser) => {
     const cart = JSON.parse(sessionStorage.getItem("shoppingBag"))
     let orderItems = []
     for (let i = 0; i < cart.length; i++) {
-        const ord = orderItems.findIndex(o => o.productId == cart[i].productId)
-        if (ord > -1)
-            orderItems[ord].quantity++;
-        else
+   //     const ord = orderItems.findIndex(o => o.productId == cart[i].productId)
+  //      if (ord > -1)
+  //          orderItems[ord].quantity++;
+  //      else
             orderItems.push({ "productId": cart[i].productId, "quantity": 1 })
     }
     const order = {
-        "userId": user,
-        "orderSum": sessionStorage.getItem("totalAmount"),
-        "orderDate": new Date(),
-        "orderItems": orderItems
+        userId: user,
+        orderSum: sessionStorage.getItem("totalAmount"),
+        orderDate: new Date(),
+        orderItemTbls: orderItems
 
     }
     return order;
 }
-//function placeOrder() {
-//    let id = sessionStorage.getItem("userId");
-//    if (!id) {
-//        alert("you must log in befor complating your order");
-//        return;
-//    }
-//    else {
-//        createOrder();
-//    }
-//}
-//async function createOrder() {
-//    try {
-//        const orderDate = new Date();
-//        const orderSum = sessionStorage.getItem("totalAmount");
-//        const userId = sessionStorage.getItem("userId");
-       
-
-       
-//        const order = { orderDate, orderSum, userId, orderItems };
-//        const res = await fetch("api/Orders", {
-//            method: "POST",
-//            headers: {
-//                "Content-Type": "application/json"
-//            },
-//            body: JSON.stringify(order)
-//        })
-//        if (!res.ok) {
-//            console.log(res)
-//            /// alert(res)
-//            return
-//        }
-//        const data = await res.json();
-//        alert(`your order ${data.orderId} created successfuly`);
-      
-
-//    }
-//    catch (er) {
-//        // alert(er)
-//    }
-//}
-//const createOrderObj = async (stringUser) => {
-//    const user = JSON.parse(stringUser)
-//    const cart = JSON.parse(sessionStorage.getItem("cart"))
-//    let orderItems = []
-//    for (let i = 0; i < cart.length; i++) {
-//        const ord = orderItems.findIndex(o => o.productId == cart[i].id)
-//        if (ord > -1)
-//            orderItems[ord].quantity++;
-//        else
-//            orderItems.push({ "productId": cart[i].id, "quantity": 1 })
-//    }
-//    const order = {
-//        "userId": user.id,
-//        "orderSum": totalPrice,
-//        "orderDate": new Date(),
-//        "orderItems": orderItems
-
-//    }
-//    return order;
-//}
 
 function Login() {
     window.location.href = "Home.html";
